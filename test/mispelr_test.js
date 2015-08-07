3bd3716adb1b914ea2a495c84323a70cee8d2d50
Michael Paulukonis <xraysmalevich@gmail.com>
2015-08-06 20:34:14 -0400

    some more tests, and temporary homophilia master-list (needs processing to js)

--- test/mispelr_test.js
+++ test/mispelr_test.js
@@ -21,33 +21,66 @@ describe("isAlpha", function() {
 });
 
 
-var getTokenTestData = function(text) {
-    return  {
-        text: text,
-        tokens: getTextArray(text)
-    };
-};
-
 describe("textArray tests with 'David23 is in da hizzouse! ", function() {
-    it("should chop up text into 7 tokens", function() {
+
+    it("getTextArray() method should exist", function() {
+        expect(typeof(getTextArray)).to.equal('function');
+    });
+
+    // wait, why the above, and nesting???
+    // if the method doesn't exist, the below test setup will fail
+    // and cause the entire test to be ignored. OUCH
+    // there must be a better way to test such things
+    // (oh, why do I want to? because of refactoring....)
+    describe("since it exists....", function() {
+
+        var getTokenTestData = function(text) {
+            return  {
+                text: text,
+                tokens: getTextArray(text)
+            };
+        };
+
         var data = getTokenTestData("David23 is in da hizzouse!");
-        expect(data.tokens.length).to.equal(7);
+
+        it("should chop up text into 7 tokens", function() {
+            expect(data.tokens.length).to.equal(7);
+        });
+
+        it("first token in the text is David", function() {
+            expect(data.tokens[0]).to.equal("David");
+        });
+
+        it("second token in the text is '23'", function() {
+            expect(data.tokens[1]).to.equal("23");
+        });
+
+        it("'!' is the last token in 'David23 is in da hizzouse!'", function() {
+            expect(data.tokens[data.tokens.length-1]).to.equal("!");
+        });
 
     });
 
-    it("first token in the text is David", function() {
-        var data = getTokenTestData("David23 is in da hizzouse!");
-        expect(data.tokens[0]).to.equal("David");
+});
+
+
+// well, this is a first stab.
+describe("maledictor tests", function() {
+
+    it("maledictor() method should exist", function() {
+        expect(typeof(maledictor)).to.equal('function');
     });
 
-    it("second token in the text is '23'", function() {
-        var data = getTokenTestData("David23 is in da hizzouse!");
-        expect(data.tokens[1]).to.equal("23");
-    });
+    describe("since it exists....", function() {
 
-    it("'!' is the last token in 'David23 is in da hizzouse!'", function() {
-        var data = getTokenTestData("David23 is in da hizzouse!");
-        expect(data.tokens[data.tokens.length-1]).to.equal("!");
-    });
+        var samples = {
+            'abbreviate': ['abreviate']
+        };
 
+        it("should replace 'abbreviate' with 'abreviate'", function() {
+            var ms = maledictor('abbreviate');
+            expect(ms).to.equal(samples['abbreviate'][0]);
+        });
+
+    });
 });
